3fbde083a122753702e0166e92a229fa60962861
Gabriel Reinoso <gabriel.reinoso.1996@gmail.com>
2024-04-03 11:23:31 -0300

    Formato impresion

--- common.js
+++ common.js
@@ -1,12 +1,12 @@
 /*const sidebarToggle = document.getElementById('menu-toggle');
 const sidebar = document.getElementById('sidebar');
 
-const divider = "-----------------------------\n";
-
 sidebarToggle.addEventListener('click', () => {
     sidebar.classList.toggle('active');
     sidebarToggle.classList.toggle('active');
 });*/
+
+const divider = "-----------------------------\n";
 
 function toBT(input) {
     var output = "rawbt:" + input;
