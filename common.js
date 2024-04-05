d7dd584095c2b8507aad734cc9c0004d103f049d
Gabriel Reinoso <gabriel.reinoso.1996@gmail.com>
2024-04-05 14:56:55 -0300

    Formato

--- common.js
+++ common.js
@@ -7,7 +7,7 @@ sidebarToggle.addEventListener('click', () => {
 });*/
 
 // Repite el caracter 29 veces para crear un divisor
-const divider = "-".repeat(29)+"\n";
+const divider = toCenter("—".repeat(14));
 
 // Imprime texto en columna izquiera y derecha, calculando el espaciado
 // Si uno de los textos supera los 14 caracteres, se corta
